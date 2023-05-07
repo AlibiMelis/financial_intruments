05ad7d2d731cd7668176af8b066507483d3bae08
Alibi Melis <melis.alibi2000@gmail.com>
2023-05-07 22:59:41 +0800

    Remove all but ios and android, connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:financial_instruments/app.dart';
+import 'package:financial_instruments/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
